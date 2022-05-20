bdd396e1ef076fbc5f26a4b77ceb4b2890318ccf
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-05-20 16:45:10 +0100

    Few small changes

--- Js/Speakers.js
+++ Js/Speakers.js
@@ -1,61 +1,64 @@
+/* eslint-disable linebreak-style */
 const speakers = [
   {
-    Name: "Kacie",
-    Web3Dao: "LearnWeb3",
-    Summary: "Kacie won his first hackathon after 2 months of web3!",
+    Name: 'Kacie',
+    Web3Dao: 'LearnWeb3',
+    Summary: 'Kacie won his first hackathon after 2 months of web3!',
     Image: {
-      url: "./Images/web3.gif",
-      alt: "Web3 BOSS",
+      url: './Images/web3.gif',
+      alt: 'Web3 BOSS',
     },
   },
   {
-    Name: "K42",
-    Web3Dao: "Web3 creator/developer",
-    Summary: "Its all data waves, lets surf the network",
+    Name: 'K42',
+    Web3Dao: 'Web3 creator/developer',
+    Summary: 'Its all data waves, lets surf the network!',
     Image: {
-      url: "./Images/K42.gif",
-      alt: "Web3 BOSS",
+      url: './Images/K42.gif',
+      alt: 'Web3 BOSS',
     },
   },
   {
-    Name: "Metalisa",
-    Web3Dao: "Web3 creator/developer",
-    Summary: "Metalist is an amazing artist and a bright soul",
+    Name: 'Metalisa',
+    Web3Dao: 'Web3 creator/developer',
+    Summary: 'Metalisa is an amazing artist and a bright soul',
     Image: {
-      url: "./Images/Eye.png",
-      alt: "Web3 BOSS",
+      url: './Images/Eye.png',
+      alt: 'Web3 BOSS',
     },
   },
   {
-    Name: "Datadigit",
-    Web3Dao: "Web3 user",
-    Summary: "Datadigit spends 142 ETH a day!",
+    Name: 'Datadigit',
+    Web3Dao: 'Web3 user',
+    Summary: 'Datadigit spends 142 ETH a day!',
     Image: {
-      url: "./Images/Viking.png",
-      alt: "Web3 BOSS",
+      url: './Images/Viking.png',
+      alt: 'Web3 BOSS',
     },
   },
   {
-    Name: "Vik",
-    Web3Dao: "Dao creator",
-    Summary: "Vik is a owner of a VVC and a DAO creator",
+    Name: 'Vik',
+    Web3Dao: 'Dao creator',
+    Summary: 'Vik is a owner of a VVC and a DAO creator',
     Image: {
-      url: "./Images/vik.png",
-      alt: "Web3 BOSS",
+      url: './Images/vik.png',
+      alt: 'Web3 BOSS',
     },
   },
   {
-    Name: "Web3 whale",
-    Web3Dao: "Lets buy NFTS",
-    Summary: "I buy NFTS",
+    Name: 'Web3 whale',
+    Web3Dao: 'Lets buy NFTS',
+    Summary: 'I buy NFTS',
     Image: {
-      url: "./Images/whale.png",
-      alt: "Web3 BOSS",
+      url: './Images/whale.png',
+      alt: 'Web3 BOSS',
     },
   },
 ];
 
-function createSpeakerCard({ Name, Web3Dao, Summary, Image }) {
+function createSpeakerCard({
+  Name, Web3Dao, Summary, Image,
+}) {
   return `
   <div class="speaker">
     <img
@@ -73,11 +76,11 @@ function createSpeakerCard({ Name, Web3Dao, Summary, Image }) {
 }
 
 function showSpeakers(speakers) {
-  const speakersContainerEl = document.querySelector("#speakers");
+  const speakersContainerEl = document.querySelector('#speakers');
 
   if (!speakersContainerEl) return;
 
-  speakersContainerEl.innerHTML = "";
+  speakersContainerEl.innerHTML = '';
 
   speakers.forEach((speaker) => {
     speakersContainerEl.innerHTML += createSpeakerCard(speaker);
@@ -85,7 +88,7 @@ function showSpeakers(speakers) {
 }
 
 const init = () => {
-  const button = document.querySelector(".see-more");
+  const button = document.querySelector('.see-more');
   const isMobile = window.innerWidth < 768;
   const displayedSpeakers = isMobile ? speakers.slice(0, 2) : speakers;
 
@@ -93,9 +96,9 @@ const init = () => {
 
   if (!button) return;
 
-  button.addEventListener("click", () => {
+  button.addEventListener('click', () => {
     showSpeakers(speakers);
-    button.style.display = "none";
+    button.style.display = 'none';
   });
 };
 
